13211e31d1ab99c3da724c85b330715af124ce50
Nicholas Fleck <nicholasfleck@gmail.com>
2015-11-05 01:00:56 -0600

    Simplify the Sieve<T> class

--- Rant/Vocabulary/Sieve.cs
+++ Rant/Vocabulary/Sieve.cs
@@ -7,69 +7,51 @@ namespace Rant.Vocabulary
 	internal class Sieve<T>
 	{
 		private readonly T[] _mainArray;
-		private readonly T[] _a;
-		private readonly T[] _b;
-		private bool stage = true;
-		private int _sizeA, _sizeB;
+		private T[] _a;
+		private T[] _b;
+		private int _size;
 
 		public Sieve(IEnumerable<T> collection)
 		{
 			if (collection == null) throw new ArgumentNullException(nameof(collection));
 			_mainArray = collection.ToArray();
-			Array.Copy(_mainArray, _a, _mainArray.Length);
-			_sizeA = _mainArray.Length;
+			_size = _mainArray.Length;
 			_a = new T[_mainArray.Length];
 			_b = new T[_mainArray.Length];
+			Array.Copy(_mainArray, _a, _mainArray.Length);
 		}
 
 		public int OriginalLength => _mainArray.Length;
 
-		public int Length => stage ? _sizeA : _sizeB;
+		public int Length => _size;
 
 		public bool Filter(Func<T, bool> predicate)
 		{
 			if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 			int n = 0;
-			if (stage)
+			// Filter operations sift passing items from one array to the other.
+			// Then the destination array is the one that is indexed.
+			// The arrays are swapped after each filter.
+			for (int i = 0; i < _size; i++)
 			{
-				for (int i = 0; i < _sizeA; i++)
-				{
-					if (predicate(_a[i])) _b[n++] = _a[i];
-				}
-				_sizeB = n;
+				if (predicate(_a[i])) _b[n++] = _a[i];
 			}
-			else
-			{
-				for (int i = 0; i < _sizeB; i++)
-				{
-					if (predicate(_b[i])) _a[n++] = _b[i];
-				}
-				_sizeA = n;
-			}
-			stage = !stage;
-			return Length > 0;
+			_size = n;
+			var swap = _b;
+			_b = _a;
+			_a = swap;
+			return _size > 0;
 		}
 
-		public void Reset()
-		{
-			stage = true;
-			_sizeA = _mainArray.Length;
-		}
+		public void Reset() => _size = _mainArray.Length;
 
 		public T this[int i]
 		{
 			get
 			{
 				if (i < 0) throw new ArgumentOutOfRangeException(nameof(i), "Index must be non-negative and less than the size of the collection.");
-				if (stage)
-				{
-					if (i >= _sizeA)
-						throw new ArgumentOutOfRangeException(nameof(i), "Index must be non-negative and less than the size of the collection.");
-					return _sizeA == _mainArray.Length ? _mainArray[i] : _a[i];
-				}
-				if (i >= _sizeB)
-					throw new ArgumentOutOfRangeException(nameof(i), "Index must be non-negative and less than the size of the collection.");
-				return _sizeB == _mainArray.Length ? _mainArray[i] : _b[i];
+				if (i >= _size) throw new ArgumentOutOfRangeException(nameof(i), "Index must be non-negative and less than the size of the collection.");
+				return _size == _mainArray.Length ? _mainArray[i] : _a[i];
 			}
 		}
 	}
