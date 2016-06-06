d917a6225bdb0aa91d1fc023aba5d06dc56aa811
Nicholas Fleck <nicholasfleck@gmail.com>
2016-06-06 13:44:24 -0500

    Further extend instruction set

--- Rant/Internal/VM/Instructions/OpCodes.cs
+++ Rant/Internal/VM/Instructions/OpCodes.cs
@@ -1,6 +1,6 @@
 ﻿namespace Rant.Internal.VM.Instructions
 {
-	internal enum OpCodes
+	internal enum OpCodes : byte
 	{
 		/// <summary>
 		/// Debug-mode data provider. Sets the current position within the source code at which the next instruction takes place.
@@ -79,5 +79,90 @@
 		/// <para>pushnum [double value]</para>
 		/// </summary>
 		PushNum = 0x0e,
+		/// <summary>
+		/// Pushes a subroutine pointer into the scope, marking it as available.
+		/// <para>pushsub [int sub_id]</para>
+		/// </summary>
+		PushSub = 0x0f,
+		/// <summary>
+		/// Removes the specified amount of subroutines from the scope.
+		/// <para>popsubs [int count]</para>
+		/// </summary>
+		PopSubs = 0x10,
+		/// <summary>
+		/// Calls the specified subroutine.
+		/// <para>callsub [int sub_id]</para>
+		/// </summary>
+		CallSub = 0x11,
+		/// <summary>
+		/// Assigns a string constant to a subroutine argument register.
+		/// <para>argstr [int table_index] [int arg_id]</para>
+		/// </summary>
+		SubArgStringConstant = 0x12,
+		/// <summary>
+		/// Assigns a number constant to a subroutine argument register.
+		/// <para>argnum [double value] [int arg_id]</para>
+		/// </summary>
+		SubArgNumberConstant = 0x13,
+		/// <summary>
+		/// Assigns a pointer constant to a subroutine argument register. Used for callbacks.
+		/// <para>argptr [int ptr] [int arg_id]</para>
+		/// </summary>
+		SubArgPtr = 0x14,
+		/// <summary>
+		/// Pops the last object from the stack and assigns it to a subroutine argument register.
+		/// <para>argobj [int arg_id]</para>
+		/// </summary>
+		PopToArgument = 0x15,
+		/// <summary>
+		/// Pushes the value of the specified argument to the stack.
+		/// <para>pusharg [int arg_id]</para>
+		/// </summary>
+		PushArgument = 0x16,
+		/// <summary>
+		/// Redirects execution to the specified address.
+		/// <para>jmp [int address]</para>
+		/// </summary>
+		Jump = 0x17,
+		/// <summary>
+		/// Pops the last two values off the stack and pushes their sum to the stack.
+		/// <para>add</para>
+		/// </summary>
+		Add = 0x18,
+		/// <summary>
+		/// Pops the last two values off the stack, subtracts the topmost value from the one below it, and pushes the result to the stack.
+		/// <para>sub</para>
+		/// </summary>
+		Subtract = 0x19,
+		/// <summary>
+		/// Pops the last two values off the stack and pushes their product to the stack.
+		/// <para>mul</para>
+		/// </summary>
+		Multiply = 0x1a,
+		/// <summary>
+		/// Pops the last two values off the stack, divides the second value by the first one, and pushes the result to the stack.
+		/// <para>div</para>
+		/// </summary>
+		Divide = 0x1b,
+		/// <summary>
+		/// Swaps the values of the two specified locals.
+		/// <para>swap [int loc1] [int loc2]</para>
+		/// </summary>
+		Swap = 0x1c,
+		/// <summary>
+		/// Pops the last two values off the stack as strings, concatenates the first value to the second one, and pushes the resulting string to the stack.
+		/// <para>concat</para>
+		/// </summary>
+		Concat = 0x1d,
+		/// <summary>
+		/// Creates a copy of the specified local and pushes it to the stack.
+		/// <para>ldloc [int index]</para>
+		/// </summary>
+		LoadLocal = 0x1e,
+		/// <summary>
+		/// Pops the topmost value off the stack and stores it in the specified local.
+		/// <para>stloc [int index]</para>
+		/// </summary>
+		SetLocal = 0x1f,
 	}
 }
